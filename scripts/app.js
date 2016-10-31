f549c1ac4d39828e219967a79a6f67c14beb4150
Daniel Roach <dannycr09@hotmail.com>
2016-10-31 09:26:22 -0400

    Brainstorming layout and possible directives, modals, and views that may be needed

--- scripts/app.js
+++ scripts/app.js
@@ -7,9 +7,32 @@ app.config(['$routeProvider', function ($routeProvider) {
             templateUrl: '',
             controller: ''
         })
-        .when('/',{
-            templateUrl: '',
+        .when('/businessDashboard',{
+            templateUrl: 'businessdashboard.html',
+            controller: ''
+        })
+        .when('/businessReviews', {
+            templateUrl: 'businessreviews.html',
+            controller: ''
+        })
+        .when('productview', {
+            templateUrl: 'productview.html',
+            controller: ''
+        })
+        .when('search', {
+            templateUrl: 'searchresults.html',
             controller: ''
         })
         .otherwise({ redirectTo: '/home'});
 }]);
+
+/*
+    Potential directives
+    - Product/Business reviews
+    - Navigation bar???
+    - Business pages left most view (Name of business, address, phone #, website
+
+    Potential modals
+    - Business - delete a product from its product listings
+    - Replying to a review (show the review in the modal as well)
+ */
