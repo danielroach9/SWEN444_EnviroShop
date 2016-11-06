a91b9af715f31f96aa186e3a1e7c792a5a4e920c
ip2176 <ip2176@g.rit.edu>
2016-11-06 16:57:57 -0500

    -Login button recognizes text values for logging in -background image fixed -divs for forms fixed -Login redirects to appropriate pages

--- client/scripts/HomeCtlr.js
+++ client/scripts/HomeCtlr.js
@@ -1,6 +1,18 @@
 angular.module('EnviroShop')
     /*.service('DataService', function () {})*/
-    .controller('HomeCtlr', ['$scope', function ($scope) {
+    .controller('HomeCtlr', ['$scope', '$location', function ($scope, $location) {
 
         console.log("HomeCtlr initialized");
+        $scope.loginCredentials = {};
+        $scope.nextPage;
+
+        $scope.login = function () {
+            if ($scope.loginCredentials.email == "wegmans@gmail.com" && $scope.loginCredentials.pswd == "123456") {
+                //$scope.nextPage =
+                $location.url('/businessDashboard');
+            }
+            else if ($scope.loginCredentials.email == "enviroshopUser@gmail.com" && $scope.loginCredentials.pswd == "123456") {
+                $location.url('/shopperDashboard');
+            }
+        };
     }]);
